6cabdee3a4543f02b195c1abf5e813cfffa07b7d
Iván Martín <ivmg19@gmail.com>
2016-05-01 15:05:03 +0200

    Fixed Login activity and added to Manifest

--- app/src/main/java/com/makersupv/makers/LoginActivity.java
+++ app/src/main/java/com/makersupv/makers/LoginActivity.java
@@ -11,6 +11,7 @@ package com.makersupv.makers;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,7 +21,7 @@ import com.parse.ParseException;
 
 import com.parse.ParseUser;
 
-public class Login extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     EditText etUsername, etPassword;
     Button btLogin;
@@ -37,19 +38,21 @@ public class Login extends AppCompatActivity {
         etPassword = (EditText) findViewById(R.id.etPassword);
         btLogin = (Button) findViewById(R.id.btLogin);
 
-        username = etUsername.getText().toString();
-        password = etPassword.getText().toString();
-
         btLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                username = etUsername.getText().toString();
+                password = etPassword.getText().toString();
 
                 ParseUser.logInInBackground(username, password, new LogInCallback() {
                     public void done(ParseUser user, ParseException e) {
                         if (user != null) {
                             nextActivity();
+                            Log.d("LOGIN", "ACCOMPLISHED");
                         } else {
                             // Signup failed. Look at the ParseException to see what happened.
+                            Log.d("LOGIN", e.getMessage());
                         }
                     }
                 });
